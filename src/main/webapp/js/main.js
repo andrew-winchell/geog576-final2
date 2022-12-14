9436966a641358d7daae3bfa1354e7a352d84fcf
Drew2134 <a.winchell1896@gmail.com>
2022-12-14 11:14:04 -0600

    Generate docx report

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -285,7 +285,7 @@ require([
             let e = [];
             e.push({name: "tab_id", value: "1"})
             $.ajax({
-                url: 'src/main/java/org/webproject/geog576final/HttpServlet.java',//'HttpServlet',
+                url: 'HttpServlet',
                 type: 'POST',
                 data: e,
                 success: (events) => {
